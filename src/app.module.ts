5adc7b35e36f65b59b5621ef040175d5fbf677a3
syariful umam <syarifulumam7@gmail.com>
2025-02-18 09:39:06 +0700

    Add : schema database prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PrismaService } from './common/prisma/prisma.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
